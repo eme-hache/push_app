cc7ccff0d32c85a47f5433332b429d680cb07ec3
Eme Hache <luisalfonsodsg@outlook.com>
2023-09-06 14:34:18 -0400

    feat: add local notifications configs

--- lib/presentation/blocs/notifications/notifications_bloc.dart
+++ lib/presentation/blocs/notifications/notifications_bloc.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:push_app/config/local_notifications/local_notifications.dart';
 
 import 'package:push_app/firebase_options.dart';
 import 'package:push_app/domain/entities/push_message.dart';
@@ -100,6 +101,8 @@ class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
       provisional: false,
       sound: true,
     );
+
+    await requestPermissionLocalNotifications();
 
     add(NotificationStatusChanged(settings.authorizationStatus));
   }
